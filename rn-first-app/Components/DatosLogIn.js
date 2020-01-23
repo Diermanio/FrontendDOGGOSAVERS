709b9d3d757849c715469b7c1d865b0f94e6db8e
Diego <diegoand2009@hotmail.com>
2020-01-23 02:24:43 -0500

    estilizando el login

--- rn-first-app/Components/DatosLogIn.js
+++ rn-first-app/Components/DatosLogIn.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect }  from 'react';
-import { StyleSheet, Text, View, Image, TouchableHighlight, TextInput, Button} from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight, TextInput, Button, TouchableWithoutFeedback, Alert, KeyboardAvoidingView, Keyboard} from 'react-native';
 
 
 
@@ -38,19 +38,84 @@ const Banner = props =>{
     }
 
     return (
-        <View style={styles.container}>
-          <Text>LOG IN</Text>
-          <Text>Usuario:</Text>
-          <TextInput onChangeText={(nombre) => setCurrentUser(nombre)} />
-          <Text>Contraseña:</Text>          
-          <TextInput onChangeText={(nombre) => setCurrentPass(nombre)} />
-          <Button title="Ingresar" onPress={ver}/>
+        // <View style={styles.container}>
+        //   <Text>LOG IN</Text>
+        //   <Text>Usuario:</Text>
+        //   <TextInput onChangeText={(nombre) => setCurrentUser(nombre)} />
+        //   <Text>Contraseña:</Text>          
+        //   <TextInput onChangeText={(nombre) => setCurrentPass(nombre)} />
+        //   <Button title="Ingresar" onPress={ver}/>
+        // </View>
+        <KeyboardAvoidingView style={styles.containerView} behavior="padding">
+
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.loginScreenContainer}>
+          <View style={styles.loginFormView}>
+          <Text style={styles.logoText}>Instamobile</Text>
+            <TextInput placeholder="Username" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} />
+            <TextInput placeholder="Password" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} secureTextEntry={true}/>
+            <Button
+              buttonStyle={styles.loginButton}
+              onPress={ver}
+              title="Login" 
+            />
+            {/* <Button
+              buttonStyle={styles.fbLoginButton}
+              onPress={() => this.onFbLoginPress()}
+              title="Login with Facebook"
+              color="#3897f1"
+            /> */}
+          </View>
         </View>
+      </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+
   );}
 
 
 
 const styles = StyleSheet.create({
+  containerView: {
+    flex: 1,
+  },
+  loginScreenContainer: {
+    flex: 1,
+  },
+  logoText: {
+    fontSize: 40,
+    fontWeight: "800",
+    marginTop: 150,
+    marginBottom: 30,
+    textAlign: 'center',
+  },
+  loginFormView: {
+    flex: 1
+  },
+  loginFormTextInput: {
+    height: 43,
+    fontSize: 14,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#eaeaea',
+    backgroundColor: '#fafafa',
+    paddingLeft: 10,
+    marginLeft: 15,
+    marginRight: 15,
+    marginTop: 5,
+    marginBottom: 5,
+  
+  },
+  loginButton: {
+    backgroundColor: '#3897f1',
+    borderRadius: 5,
+    height: 45,
+    marginTop: 10,
+  },
+  fbLoginButton: {
+    height: 45,
+    marginTop: 10,
+    backgroundColor: 'transparent',
+  },
   banner: {
     flexDirection: "row",
     justifyContent: 'space-evenly',
